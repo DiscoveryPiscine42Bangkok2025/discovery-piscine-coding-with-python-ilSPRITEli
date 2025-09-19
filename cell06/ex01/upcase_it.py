21116d3f38a90e797970dfd91bbb99754fdb5bbf
ไข่หมา <65070020@kmitl.ac.th>
2025-09-19 22:51:54 +0700

    refactor

--- cell06/ex01/upcase_it.py
+++ cell06/ex01/upcase_it.py
@@ -2,7 +2,7 @@ class UpcaseIt:
     def upcase(self, text: str) -> str:
         return text.upper()
 
-up = UpcaseIt().upcase
-print(up("hello"))
+upcase_it = UpcaseIt().upcase
+print(upcase_it("hello"))
 
 
